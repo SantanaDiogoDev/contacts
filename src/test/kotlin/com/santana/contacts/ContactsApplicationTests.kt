485b2241c0f46295634c09e6cd049acc58ba29aa
SantanaDiogoDev <diogo.santana.dev@gmail.com>
2023-10-21 22:29:51 -0300

    projeto inicial API kotlin

---
+++ src/test/kotlin/com/santana/contacts/ContactsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.santana.contacts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
